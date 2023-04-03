fa4891e6b91e001700e0b3d524bf3767fbcbf0cc
an-rybacka <121604351+an-rybacka@users.noreply.github.com>
2023-04-03 16:03:02 +0200

    Structure cleaning

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <footer>
+        This project was coded by Aneta Rybacka with{" "}
+        <a href="https://react.dev/" target="_blank">React</a> and is{" "}
+        <a href="https://github.com/an-rybacka/weather-react" target="_blank">
+          open-sourced on GitHub
+        </a>{" "}
+      </footer>
     </div>
   );
 }
